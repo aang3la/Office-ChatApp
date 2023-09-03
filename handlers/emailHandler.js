473a5271556246c16f741800a4ff706c18cafc9a
Angela Antova <antovaangela@gmail.com>
2023-09-03 20:30:55 +0200

    changes

--- handlers/emailHandler.js
+++ handlers/emailHandler.js
@@ -1,36 +1,40 @@
+//! npm install nodemailer
 const nodemailer = require("nodemailer");
 
-exports.sendEmail = async (req, res) => {
-    try{
-        //* 1) Creating the transport with mailtrap
-        const transporter = nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            auth: {
-                user: process.env.EMAIL_ADDRESS,
-                pass: process.env.EMAIL_PASSWORD
-            }
-        });
-        transporter.verify((err, succ) => {
-            if(err){
-                console.log(err);
-            } else {
+const sendEmail = async (options) => {
+  try {
+    //* 1) Creating the transporter
 
-                console.log('Success!');
-            }
-        });
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
 
-        //* 2) Define the options for the email
-        const mailOptions = {
-            from: "Office Chat App <office.app@gmail.com>",
-            to: options.email,
-            subject: options.subject,
-            text: options.message
-        };
-
-        //* 3) Sending the email
-        await transporter.sendMail(mailOptions);
-    }catch(err){
+    transporter.verify((err, succ) => {
+      if (err) {
         console.log(err);
-    }
+      } else {
+        console.log("success");
+      }
+    });
+
+    //* 2) Defining the options of the email
+    const mailOptions = {
+      from: "Office Chat App <officeapp@hotmail.com>",
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
+
+    //* 3) Sending the email
+    await transporter.sendMail(mailOptions);
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+module.exports = sendEmail;
